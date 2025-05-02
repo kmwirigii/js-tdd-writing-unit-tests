8c6e39cb593ee7a8cf24daf78d2b0be6b705131e
Kelvin Mwirigi <kmwirigi05@gmail.com>
2025-05-02 23:03:33 +0300

    commit

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,23 @@
-// Your code here
+function isPalindrome(word) {
+    if (typeof word !== 'string') {
+      throw new Error('Input must be a string');
+    }
+  
+    if (word === '') {
+      return false; // Handle the empty string case separately
+    }
+  
+    if (!/^[A-Za-z]+$/.test(word)) {
+      throw new Error('Input must only contain alphabetic characters');
+    }
+  
+    // Convert to lowercase to make the function case-insensitive
+    const normalizedWord = word.toLowerCase();
+    const reversedWord = normalizedWord.split('').reverse().join('');
+    
+    return normalizedWord === reversedWord;
+  }
+  
+  module.exports = { isPalindrome };
+  
+  
